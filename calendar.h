f4d0c388ec9d5f30c88337709928d0cb73d0d27a
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-12-13 06:49:53 -0500

    Update calendar.h

--- calendar.h
+++ calendar.h
@@ -5,21 +5,45 @@ using namespace std;
 
 class Calendar{
   private:
-    string name;
+    string name, type;
+    int fm, fd, fy;
   
   public:
     Calendar() {
       name = " ";
+      type = " ";
+      fm = 0;
+      fd = 0;
+      fy = 0;
     }
     
-    Calendar(string n){
+    Calendar(string n, string t, int m, int d, int y){
       name = n;
+      type = t;
+      fm = m;
+      fd = d;
+      fy = y;
     }
 
     void setName(string n)
       {name = n;}
-
+    void setType(string t)
+      {type = t;}
+    void setMonth(int m)
+      {fm = m;}
+    void setDay(int d)
+      {fd =d;}
+    void setYear(int y)
+      {fy = y;}
     string getName() const
       {return name;}
+    string getType() const
+      {return type;}
+    int getMonth() const
+      {return fm;}
+    int getDay() const
+      {return fd;}
+    int getYear() const
+      {return fy;}
 };
 #endif
