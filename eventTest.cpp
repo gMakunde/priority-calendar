62e9d66d32f60216930e0890500d5edbfec0109f
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-12-13 00:12:54 -0500

    Update eventTest.cpp

--- eventTest.cpp
+++ eventTest.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <string>
 #include "event.h"
+#include "calendar.h"
 using namespace std;
 
-int amountofDays(int, int, int);
-int importanceLevel(int, string);
-int fm, fd, fy, counter;
-
 int main() {
+  int fm, fd, fy, counter;
   int month, day, year, daysLeft, imp;
   string Type, name;
   Event list;
@@ -25,7 +23,7 @@ int main() {
   cin.ignore();
   getline(cin, name);
   do{
-    cout << "What type of event is it? (Holiday, Birthday, Assignment, Test: " << endl;
+    cout << "What type of event is it? (Holiday, Birthday, Assignment, Test): " << endl;
     getline(cin, Type);
 
     cout << "Enter the month of the event: " << endl;
@@ -51,82 +49,3 @@ int main() {
   list.displayList();
 }
 
-int importanceLevel(int daysL, string TypeofEvent){
-  int importance = 0;
-  if(TypeofEvent == "Test" || TypeofEvent == "test"){
-    importance = 5;
-  }
-  if(TypeofEvent == "Assignment" || TypeofEvent == "assignment"){
-    importance = 4;
-  }
-  if(TypeofEvent == "Birthday" || TypeofEvent == "birthday"){
-      importance = 2;
-  }
-  if(TypeofEvent == "Holiday" || TypeofEvent == "holiday"){
-    importance = 1;
-  }
-
-  if(daysL > 30){
-    importance = 1;
-  }
-  if(daysL > 21 && daysL <= 30){
-    importance = importance + 2;
-  }
-  if(daysL > 14 && daysL <= 21){
-    importance = importance + 3;
-  }
-  if(daysL > 7 && daysL <= 14){
-    importance = importance + 4;
-  }
-  if(daysL > 0 && daysL <= 7){
-    importance = importance + 5;
-  }
-
-  return importance;
-}
-
-int amountofDays(int m, int d, int y){
-  //If the day is the same as the day of the event, RETURN THE AMOUNT OF DAYS BETWEEN
-  if(fm == m && fd == d && fy == y)
-    return counter;
-
-  //INCREMENT THE DAY AND AMOUNT OF DAYS UNTIL THE EVENT
-  d++;
-  counter++;
-
-  //(LEAP YEAR) CHECKS DAY ACCORDING TO MAKE SURE THE DAY RESTARTS IF REACHES OUT OF MONTH
-  if(y % 4 == 0){
-    if (m == 2)
-      if (d == 30){
-        m++;
-        d = 1;
-      }
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-      if (d == 32){
-        m++; 
-        d = 1;
-      }
-    if (m == 4 || m == 6 || m == 9 || m == 11)
-      if (d == 31){
-        m++;
-        d = 1;
-      }
-  }else{
-    if (m == 2)
-      if (d == 29){
-        m++;
-        d = 1;
-      }
-    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-      if (d == 32){
-        m++; 
-        d = 1;
-      }
-    if (m == 4 || m == 6 || m == 9 || m == 11)
-      if (d == 31){
-        m++;
-        d = 1;
-      }
-  }
-  return amountofDays(m, d, y);
-}
