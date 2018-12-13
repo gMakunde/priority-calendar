f616cccaf018bd7353a4809ad4a40e451da6d5f5
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-12-13 06:48:45 -0500

    Update eventTest.cpp

--- eventTest.cpp
+++ eventTest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include<fstream>
 #include "event.h"
+#include "assigncal.h"
 #include "calendar.h"
 using namespace std;
 
@@ -8,46 +10,74 @@ int main() {
   int fm, fd, fy, counter;
   int month, day, year, daysLeft, imp;
   string Type, name;
-  Event list;
+  Assignment a;
+  Calendar c;
+  Eventlist list;
 
-  cout << "Enter today's month (numerical): " << endl;
+  cout << "Enter today's month, day and year (numerical): " << endl;
   cin >> month;
-  cout << "Enter today's day: " << endl;
   cin >> day;
-  cout << "Enter today's year: " << endl;
   cin >> year;
-
-
-  counter = 0;
   
   char yesno = 'y';
   while (yesno == 'y' || yesno == 'Y'){
-    cout << "What is the name of the event: " << endl;
-    cin.ignore();
-    getline(cin, name);
-    cout << "What type of event is it? (Holiday, Birthday, Assignment, Test): " << endl;
-    getline(cin, Type);
+    cout << "Import event from file?(y/n)" << endl;
+    cin >> yesno;
+    if (yesno == 'y' || yesno == 'Y'){
+      ifstream file;
+      file.open("events.txt");
+      if(file.is_open()){
+        int size;
+        cout << "How Many events are on the file?" << endl;
+        cin >> size;
+        for(int i=0; i<size; i++){
+          file >> name >> Type >> fm >> fd >> fy;
+          
+          
+          daysLeft = amountofDays(month, day, year, fm, fd, fy, 0);
+          imp = importanceLevel(daysLeft, Type);
+          list.insert(name, Type, imp, daysLeft);
+        }
+      }
+      file.close();
 
-    cout << "Enter the month of the event: " << endl;
-    cin >> fm;
-    cout << "Enter the day of the event: " << endl;
-    cin >> fd;
-    cout << "Enter the year of the event: " << endl;
-    cin >> fy;
-    cout << "hi" << endl;
-    daysLeft = amountofDays(month, day, year);
+    }else{
+      cout << "What is the name of the event: " << endl;
+      cin.ignore();
+      getline(cin, name);
+      cout << "What type of event is it? (Holiday, Birthday, Assignment, Test): " << endl;
+      getline(cin, Type);
+      cout << "Enter Event month day and year: " << endl;
+      cin >> fm;
+      cin >> fd;
+      cin >> fy;
+      Calendar(name, Type, fm, fd, fy);
+      
+      
+    
+    daysLeft = amountofDays(month, day, year, fm, fd, fy, 0);
     imp = importanceLevel(daysLeft, Type);
-    list.enqueue(name, Type, imp, daysLeft);
-
+    list.insert(name, Type, imp, daysLeft);
+    }
     cout << "Enter another event? (y/n)" << endl;
     cin >> yesno;
   }
   cout << endl << endl << "YOUR PRIORITY LIST:" << endl;
+  list.displayList();
   string value1, value2;
   int value3, value4;
-  while (!list.isEmpty()){
-    list.dequeue(value1, value2, value3, value4);
-    cout << value1 << " " << value2 << " " << value3 << " " << value4 << endl;
-  }
+  //fstream file;
+  //cout << "To saving events to file event.txt..." << endl;
+
+  //file.open("event.txt", ios::app);
+  //if(file.is_open()){
+  //  while (list){
+  //    list.(value1, value2, value3, value4);
+  //    file << value1 << " " << value2 << " " << value3 << " " << value4 << endl;
+  //  }
+  //}
+  //file.close();
+  
+  return 0;
 }
 
