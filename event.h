8129567aba929d5382d01f9b78f650713ecb09e4
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-12-13 01:06:08 -0500

    Update event.h

--- event.h
+++ event.h
@@ -9,74 +9,25 @@ int importanceLevel(int, string);
 
 class Event{
   private:
-    struct ListNode{
+    struct QueueNode{
       string Type, name;
       int importance;
       int days;
-      struct ListNode *next;
+      QueueNode *next;
     };
-    ListNode *head;
-  public:
-    Event()
-    {head = nullptr;}
-    void setattributes(string type, int imp, int days){
-      ListNode newnode;
-      newnode.Type = type;
-      newnode.importance = imp;
-      newnode.days = days;
-    }
-    void insert(string name, string type, int imp, int days){
-      ListNode *newnode;
-      newnode = new ListNode;
-      newnode->name = name, newnode->Type = type, newnode->importance = imp, newnode->days = days, newnode->next = nullptr;
-      ListNode *nodeptr;
-      ListNode *prevNode;
-      prevNode = new ListNode;
-      prevNode = nullptr;
-      nodeptr = head;
-
-        while(nodeptr && nodeptr->importance >= newnode->importance){
-          prevNode = nodeptr;
-          nodeptr = nodeptr->next;
-        }
-        if(prevNode == nullptr){
-          head = newnode;
-          newnode->next = nodeptr;
-        }else{
-          prevNode->next = newnode;
-          newnode->next = nodeptr;
-        }
-
-    }
-    void insertNode(string name, string type, int imp, int days){
-      ListNode *newnode;
-     
-
-      newnode = new ListNode;
-      newnode->name = name;
-      newnode->Type = type;
-      newnode->importance = imp;
-      newnode->days = days;
-      newnode->next = nullptr;
-
-      if(!head){
-        head = newnode;
-        newnode->next = nullptr;
-      }else{
-        insert(name,type,imp,days);
-      }
-    }
-    void displayList() const{
-      ListNode *nodeptr;
-      nodeptr = head;
-
-      while(nodeptr){
-          cout << nodeptr->name << ": " << nodeptr->Type << " is " << nodeptr->days << " days away from today." << endl;
-          nodeptr = nodeptr->next;
-      }
-    }
-    
+    QueueNode *front;
+    QueueNode *rear;
+    int numItems;
   
+  public:
+    Event();
+
+    ~Event();
+
+    void enqueue(string, string, int, int);
+    void dequeue(string &, string &, int &, int &);
+    bool isEmpty() const;
+    void clear();
 };
 
 #endif
