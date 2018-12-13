cec33b8cecd9adfc0ac8f71d9e912bb51f19370b
georgeravensm <34068912+georgeravensm@users.noreply.github.com>
2018-12-13 14:42:47 -0500

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -1,21 +1,164 @@
 #include <iostream>
 #include <string>
+#include<fstream>
 #include "event.h"
+#include "derivecalendar.h"
+#include "calendar.h"
 using namespace std;
 
+void todo(){
+  int placeholder = 0;
+}
+void prioritzer(){
+  int fm, fd, fy, counter;
+  int month, day, year, imp;
+  string Type, name, attribute;
+  Assignment a;
+  Holiday h;
+  Birthday b;
+  Meeting m;
+  Test t;
+  Errand e;
+  Calendar c;
+  Eventlist list;
 
+  cout << "Enter today's month, day and year (numerical): " << endl;
+  cin >> month;
+  cin >> day;
+  cin >> year;
+
+  char yesno = 'y';
+  while (yesno == 'y' || yesno == 'Y'){
+    cout << "Import event from file?(y/n)" << endl;
+    cin >> yesno;
+    if (yesno == 'y' || yesno == 'Y'){
+      ifstream file;
+      file.open("events.txt");
+      if(file.is_open()){
+        int size;
+        cout << "How Many events are on the file?" << endl;
+        cin >> size;
+        for(int i=0; i<size; i++){
+          file >> name >> Type >> fm >> fd >> fy >> attribute;
+          Calendar(name,Type,fm,fd,fy);
+          if (Type == "Holiday" || Type == "holiday" || Type == "h"){
+            h.setFestivity(attribute);
+          }
+          else if(Type == "Test" || Type == "test" || Type == "t"){
+            t.setSubject(attribute);
+          }
+          else if(Type == "Assignment" || Type == "assignment" || Type == "a"){
+            a.setSubject(attribute);
+          }
+          else if(Type == "meeting" || Type == "Meeting" || Type== "m"){
+            m.setGroup(attribute);
+          }
+          else if(Type == "errand" || Type == "Errand" || Type == "e"){
+            e.setType(attribute);
+          }
+          else if(Type == "Birthday" || Type == "birthday" || Type == "b"){
+            b.setAge(attribute);
+          }
+          //daysLeft = amountofDays(month, day, year, fm, fd, fy, 0);
+          //imp = importanceLevel(daysLeft, Type);
+          //list.insert(name, Type, imp, daysLeft, attribute);
+        }
+      }
+      file.close();
+
+    }else{
+      cout << "What is the name of the event: " << endl;
+      cin.ignore();
+      getline(cin, name);
+      cout << "What type of event is it? (Holiday, Birthday, Assignment, Test, Meeting, Errand): " << endl;
+      getline(cin, Type);
+      cout << "Enter Event month day and year: " << endl;
+      cin >> fm;
+      cin >> fd;
+      cin >> fy;
+      cout<<"check 1" << endl;
+      if (Type == "Holiday" || Type == "holiday" || Type == "h"){
+        cout << "Enter Holiday Festivity"<< endl;;
+        cin.ignore();
+        getline(cin, attribute);
+          }
+      else if(Type == "Test" || Type == "test" || Type == "t"){
+        cout << "Enter Test Subject"<< endl;;
+        cin.ignore();
+        getline(cin, attribute);
+      }
+      else if(Type == "Assignment" || Type == "assignment" || Type == "a"){
+        cout << "Enter Subject of Assignment"<< endl;;
+        cin.ignore();
+        getline(cin, attribute);
+      }
+      else if(Type == "meeting" || Type == "Meeting" || Type== "m"){
+        cout << "Enter Group you are meeting with"<< endl;;
+        cin.ignore();
+        getline(cin, attribute);
+      }
+      else if(Type == "errand" || Type == "Errand" || Type == "e"){
+        cout << "Enter Type of Errand" << endl;
+        cin.ignore();
+        getline(cin, attribute);
+      }
+      else if(Type == "Birthday" || Type == "birthday" || Type == "b"){
+        cout << "Enter Age they/you are turning"<< endl;;
+        cin.ignore();
+        getline(cin, attribute);
+      }
+      cout << "check check" << endl;
+      CalcImportanceandInsert(month, day, year, fm, fd, fy, name, Type, attribute);
+      
+    }
+    cout << "Enter another event? (y/n)" << endl;
+    cin >> yesno;
+  }
+  cout << endl << endl << "YOUR PRIORITY LIST:" << endl;
+  list.displayList();
+  string value1, value2;
+  int value3, value4;
+}
+
+void CalcImportanceandInsert(int month, int day, int year, int fm, int fd, int fy, string name, string Type, string attribute){
+  Assignment a;
+  Holiday h;
+  Birthday b;
+  Meeting m;
+  Test t;
+  Errand e;
+  Calendar c;
+  Eventlist list;
+  int daysLeft, imp;
+  
+  //Calendar(name, Type, fm, fd, fy);
+  daysLeft = amountofDays(month, day, year, fm, fd, fy, 0);
+  cout << "check2" << endl;
+  imp = importanceLevel(daysLeft, Type);
+  cout<<"importance:" << imp << endl;
+  cout << "check3" << endl;
+  list.insertNode(name, Type, imp, daysLeft, attribute);
+  cout << "check4" << endl;
+
+}
 int importanceLevel(int daysL, string TypeofEvent){
   int importance;
-  if(TypeofEvent == "Test" || TypeofEvent == "test"){
+  if(TypeofEvent == "Test" || TypeofEvent == "test" || TypeofEvent == "t"){
     importance = 5;
   }
-  if(TypeofEvent == "Assignment" || TypeofEvent == "assignment"){
+  if(TypeofEvent == "Assignment" || TypeofEvent == "assignment" || TypeofEvent == "a"){
     importance = 4;
   }
-  if(TypeofEvent == "Birthday" || TypeofEvent == "birthday"){
+  if(TypeofEvent == "meeting" || TypeofEvent == "Meeting" || TypeofEvent == "m"){
+    importance = 4;
+  }
+  if(TypeofEvent == "errand" || TypeofEvent == "Errand" || TypeofEvent == "e"){
+    importance = 3;
+  }
+  if(TypeofEvent == "Birthday" || TypeofEvent == "birthday" || TypeofEvent == "b"){
       importance = 2;
   }
-  if(TypeofEvent == "Holiday" || TypeofEvent == "holiday"){
+  if(TypeofEvent == "Holiday" || TypeofEvent == "holiday" || TypeofEvent == "h"){
     importance = 1;
   }
 
@@ -31,7 +174,7 @@ int importanceLevel(int daysL, string TypeofEvent){
   if(daysL > 7 && daysL <= 14){
     importance = importance + 4;
   }
-  if(daysL > 0 && daysL <= 7){
+  if(daysL >= 0 && daysL <= 7){
     importance = importance + 5;
   }
 
@@ -93,7 +236,7 @@ Event::~Event(){
   clear();
 }
 
-void Event::enqueue(string n, string t, int i, int d){
+void Event::enqueue(string n, string t, int i, int d, string ea){
   QueueNode *newNode = nullptr;
 
   newNode = new QueueNode;
@@ -101,6 +244,7 @@ void Event::enqueue(string n, string t, int i, int d){
   newNode->importance = i;
   newNode->Type = t;
   newNode->days = d;
+  newNode->eventAtrribute = ea;
   newNode->next = nullptr;
 
   if(isEmpty()){
@@ -114,7 +258,7 @@ void Event::enqueue(string n, string t, int i, int d){
   numItems++;
 }
 
-void Event::dequeue(string &n, string &t, int &i, int &d){
+void Event::dequeue(string &n, string &t, int &i, int &d, string &ea){
   QueueNode *temp = nullptr;
   if (isEmpty()){
     cout << "The queue is empty" << endl;
@@ -124,6 +268,7 @@ void Event::dequeue(string &n, string &t, int &i, int &d){
     t = front->Type;
     i = front->importance;
     d = front->days;
+    ea = front->eventAtrribute;
     temp = front;
     front = front->next;
     delete temp;
@@ -132,10 +277,10 @@ void Event::dequeue(string &n, string &t, int &i, int &d){
 }
 
 void Event::clear(){
-  string value, value2;
+  string value, value2, value5;
   int value3, value4;
   while(!isEmpty()){
-    dequeue(value, value2, value3, value4);
+    dequeue(value, value2, value3, value4, value5);
   }
 }
 
